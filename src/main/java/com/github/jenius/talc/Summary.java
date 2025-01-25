b8f284ad29f9099023dd5dee74456bfaa7420ba8
forax <forax@univ-mlv.fr>
2025-01-25 22:44:48 +0100

    remove unused imports

--- src/main/java/com/github/jenius/talc/Summary.java
+++ src/main/java/com/github/jenius/talc/Summary.java
@@ -1,12 +1,6 @@
 package com.github.jenius.talc;
 
-import com.github.jenius.component.ComponentStyle;
-import com.github.jenius.component.Node;
-import com.github.jenius.component.XML;
-
-import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 public record Summary(String title, List<String> exercises) {
