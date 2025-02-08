815169a748700b658c5a5811ed2adbba62b61e31
forax <forax@univ-mlv.fr>
2025-02-08 17:49:53 +0100

    transform diff status plan to a true test

--- src/test/java/com/github/jenius/talc/PlanTest.java
+++ src/test/java/com/github/jenius/talc/PlanTest.java
@@ -5,24 +5,40 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlanTest {
+  private static String mapping(String name) {
+    var index = name.lastIndexOf('.');
+    if (index == -1) {
+      return name;
+    }
+    var extension = name.substring(index + 1);
+    var newExtension = switch (extension) {
+      case "xumlv" -> "html";
+      default -> extension;
+    };
+    return name.substring(0, index) + "." + newExtension;
+  }
+
   @Test
-  public void test() throws IOException, URISyntaxException {
+  public void diffStatusPlan() throws IOException, URISyntaxException {
     var path = Path.of(PlanTest.class.getResource(".").toURI());
-    var planFactory = new PlanFactory(name -> {
-      var index = name.lastIndexOf('.');
-      if (index == -1) {
-        return name;
-      }
-      var extension = name.substring(index + 1);
-      var newExtension = switch (extension) {
-        case "xumlv" -> "html";
-        default -> extension;
-      };
-      return name.substring(0, index) + "." + newExtension;
-    });
-    var plan = planFactory.diff(path.resolve("root"), path.resolve("dest"));
-    System.out.println(plan);
+    var planFactory = new PlanFactory(PlanTest::mapping);
+    var root = path.resolve("root");
+    var dest = path.resolve("dest");
+    var plan = planFactory.diff(root, dest);
+
+    var statusMap = plan.statusMap();
+    assertEquals(Map.of(
+        root.resolve("index.xumlv"), new Status(Status.State.ADDED, dest.resolve("index.html")),
+        root.resolve("template.html"), new Status(Status.State.ADDED, dest.resolve("template.html")),
+        root.resolve("Java"), new Status(Status.State.ADDED, dest.resolve("Java")),
+        root.resolve("Java/index.xumlv"), new Status(Status.State.ADDED, dest.resolve("Java/index.html")),
+        root.resolve("Java/td01.xumlv"), new Status(Status.State.ADDED, dest.resolve("Java/td01.html")),
+        dest.resolve("should_be_removed.txt"), new Status(Status.State.REMOVED, dest.resolve("should_be_removed.txt"))
+    ), statusMap);
   }
 }
